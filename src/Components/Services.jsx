37044c61381b1eacdc8638ecf0c8340433d842af
alzabid <alzabid.ashik@gmail.com>
2024-05-01 13:38:11 +0600

    home page responsived

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,7 +1,9 @@
 const Services = () => {
   return (
     <>
-      <h1 className="text-center text-xl md:text-3xl lg:text-4xl font-bold py-10">Our Services</h1>
+      <h1 className="text-center text-xl md:text-3xl lg:text-4xl font-bold py-10">
+        Our Services
+      </h1>
       <div className="max-w-7xl mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {/* 1 */}
         <div className="flex items-start gap-6 p-6 max-w-96 bg-blue-50 rounded-lg">
@@ -10,7 +12,7 @@ const Services = () => {
             <h1 className="text-xl md:text-2xl font-semibold text-gray-800">
               Advanced Property Search
             </h1>
-            <p className="text-gray-500 text-justify pt-3 md:pt-6">
+            <p className="text-sm lg:text-base text-gray-500 text-justify pt-3  lg:pt-6">
               Effortlessly find your dream property with advanced search
               filters.
             </p>
@@ -23,7 +25,7 @@ const Services = () => {
             <h1 className="text-xl md:text-2xl font-semibold text-gray-800">
               Virtual Tours and Multimedia
             </h1>
-            <p className="text-gray-500 text-justify pt-3 md:pt-6">
+            <p className="text-sm lg:text-base text-gray-500 text-justify pt-3 lg:pt-6">
               Explore properties through immersive virtual tours and HD photos.
             </p>
           </div>
@@ -35,7 +37,7 @@ const Services = () => {
             <h1 className="text-xl md:text-2xl font-semibold text-gray-800">
               Secure Online Transactions
             </h1>
-            <p className="text-gray-500 text-justify pt-3 md:pt-6">
+            <p className="text-sm lg:text-base text-gray-500 text-justify pt-3  lg:pt-6">
               Ensure secure transactions and e-sign documents seamlessly online.
             </p>
           </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -53,43 +53,47 @@ const Home = () => {
           </div>
         </div>
         {/* 2 */}
-        <div className="flex flex-col md:flex-row-reverse gap-5 mt-10 mb-72">
+        <div className="flex flex-col justify-cente md:items-center lg:items-start lg:flex-row-reverse gap-20 md:gap-24 lg:gap-5 mt-10 lg:mt-20 lg:mb-72">
           {/* image */}
-          <div className="relative w-1/2">
+          <div className="relative w-9/12 md:w-full lg:w-1/2">
             <img
-              className="absolute top-24 rounded-lg"
-              src="/public/image/home1.jpg"
+              className="lg:absolute lg:top-24  rounded-lg"
+              src="/public/image/home3.jpg"
               alt=""
             />
             <img
-              className="absolute right-0 top-0 rounded-lg w-[400px]"
-              src="/public/image/home2.jpg"
+              className="absolute -right-20 md:right-0 lg:right-0 top-36 md:top-72 lg:top-0 rounded-lg w-[400px]"
+              src="/public/image/home4.jpg"
               alt=""
             />
             <img
-              className="absolute left-[205px] top-[72px] w-16 "
+              className="absolute -right-[22px] md:right-[230px]  lg:right-[140px]  top-[128px] md:top-[262px] lg:left-[205px] lg:top-[72px]  w-12 md:w-16 "
               src="/public/icon/play.png"
               alt=""
             />
           </div>
           {/* text */}
-          <div className="w-1/2">
-            <div className="flex flex-col items-center md:items-start w-9/12 ">
+          <div className="lg:w-1/2">
+            <div className="flex flex-col items-center md:items-start md:w-9/12">
               <p className=" text-[#F06711] font-medium text-center md:text-start ">
                 --- Property buying
               </p>
-              <h1 className="text-center md:text-start text-3xl font-bold my-4 ">
+              <h1 className="text-2xl md:text-3xl font-bold text-center md:text-start  my-4 ">
                 Efficient and Transparent Home Buying Solutions
               </h1>
-              <p className="text-[#667085] text-center md:text-start">
+              <p className="text-sm md:text-base text-[#667085] text-center md:text-start">
                 It is a long established fact that a reader will be distracted
-                by the readable content of a page when looking at its layout.
+                by the readable content of a page when looking at its layout. 
               </p>
-              <button className="mt-12 flex justify-center items-center gap-3 bg-[#ECF5FF] px-3 py-2 md:px-4 md:py-2 text-blue-700">
+              <button className="mt-6 lg:mt-12 flex justify-center items-center gap-3 bg-[#ECF5FF] px-3 py-2 md:px-4 md:py-2 text-blue-700">
                 <div>
-                  <img src="/public/icon/Magnifer.png" alt="" />
+                  <img
+                    className="w-4 md:w-5"
+                    src="/public/icon/Magnifer.png"
+                    alt=""
+                  />
                 </div>
-                <p>Find Properties</p>
+                <p className="text-sm md:text-base">Find Properties</p>
               </button>
             </div>
           </div>
